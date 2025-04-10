de29915998e407062443a136116dc93929479c40
Joshua-Handy <joshua.handy2112@gmail.com>
2025-04-10 00:37:58 -0500

    Added Nav Bar

--- web_app/views.py
+++ web_app/views.py
@@ -3,5 +3,6 @@ from django.http import HttpResponse
 import datetime
 # Create your views here.
 def home(request):
-    return HttpResponse("This is the home page of the To-Do List web app.")
+    return render(request, 'base.html')
+
 
